f8b0bcb348cf3ee426ec52fad28a9c5ee4ded75f
Mohd Alif Haikal Abd Razak <alifhaikal88@gmail.com>
2013-11-09 14:32:21 +0800

    added gwt module, web xml

---
+++ kpn-quiz-web/src/main/java/my/gov/kpn/quiz/web/config/QaWebSecurityConfig.java
@@ -0,0 +1,64 @@
+package my.gov.kpn.quiz.web.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+
+/**
+ * http://spring.io/blog/2013/07/03/spring-security-java-config-preview-web-security/
+ */
+@Configuration
+@EnableWebSecurity
+public class QaWebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    @Qualifier(value = "userDetailService")
+    private UserDetailsService userDetailService;
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/gxt/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .headers()
+                .addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
+                .and()
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/*").permitAll()
+                .antMatchers("/my.gov.kpn.quiz.web.Finance/**").permitAll()
+                .antMatchers("/secure/**").hasRole("USER")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/application.html?gwt.codesvr=127.0.0.1:9997")
+                .failureUrl("/index.html?login_error=1")
+                .loginPage("/index.html")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/index.html")
+                .invalidateHttpSession(true);
+    }
+
+
+    @Override
+    protected void registerAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailService);
+    }
+
+}
