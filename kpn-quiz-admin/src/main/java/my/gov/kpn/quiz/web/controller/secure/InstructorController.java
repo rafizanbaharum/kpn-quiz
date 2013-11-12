30021c837009d6b84b11b16666f9d3ef2203896c
Mohd Alif Haikal Abd Razak <alifhaikal88@gmail.com>
2013-11-12 16:41:49 +0800

    added dashboard page

--- kpn-quiz-admin/src/main/java/my/gov/kpn/quiz/web/controller/secure/InstructorController.java
+++ kpn-quiz-admin/src/main/java/my/gov/kpn/quiz/web/controller/secure/InstructorController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/secure/instructor")
+@RequestMapping("/secure/index")
 public class InstructorController {
 
     @RequestMapping(method = {RequestMethod.GET})
     public String go(ModelMap model) {
-        return "index";
+        return "secure/index";
     }
 }
