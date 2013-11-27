791c1f9dbb732ed451281433a1b2ce4fcd369ddf
Rafizan Uda Baharum <rafizan.baharum@gmail.com>
2013-11-27 12:51:57 +0000

    Fixed issue #22

--- kpn-quiz-lib/src/test/resources/data/principal.sql
+++ kpn-quiz-lib/src/test/resources/data/principal.sql
@@ -13,6 +13,11 @@ delete from QA_PCPL;
 insert into QA_PCPL (ID, NAME, PRINCIPAL_TYPE, ENABLED, LOCKED, M_ST, C_ID, C_TS) values (1, 'root', 0, true, true, 1, 0, CURRENT_TIMESTAMP );
 insert into QA_USER (ID, REALNAME, PASSWORD, EMAIL, ACTOR_ID)
 values (1, 'System Root', 'abc123', 'rafizan.baharum@gmail.com', null);
+insert into QA_ACTR (ID, ACTOR_TYPE, ADDRESS1, ADDRESS2, ADDRESS3, AGE, EMAIL, FAX, M_ST, C_ID, C_TS)
+values (0, 0, 'Lorong 1 ', 'Taman Midah', 'Cheras', 22, 'rafizan.baharum@gmail.com', 'fax', 1, 1, CURRENT_TIMESTAMP );
+insert into QA_SPPT(ID)
+values(0)
+update QA_USER set ACTOR_ID = 0 where id = 1;
 
 insert into QA_PCPL_ROLE (ID, ROLE_TYPE, PRINCIPAL_ID, M_ST, C_ID, C_TS) values(1, 0, 1, 1, 1, CURRENT_TIMESTAMP );
 insert into QA_PCPL_ROLE (ID, ROLE_TYPE, PRINCIPAL_ID, M_ST, C_ID, C_TS) values(2, 1, 1, 1, 1, CURRENT_TIMESTAMP );
