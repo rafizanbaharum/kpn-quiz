31e3be2adc2812f2839587f1bc7d7ffbf7b99061
Rafizan Uda Baharum <rafizan.baharum@gmail.com>
2013-12-02 02:44:01 +0000

    Added schema test

---
+++ kpn-quiz-lib/src/test/java/my/gov/kpn/quiz/core/QaSchemaTest.java
@@ -0,0 +1,39 @@
+package my.gov.kpn.quiz.core;
+
+import my.gov.kpn.quiz.config.Config;
+import org.hibernate.SessionFactory;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author rafizan.baharum
+ * @since 11/8/13
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {Config.class})
+public class QaSchemaTest extends AbstractTransactionalJUnit4SpringContextTests {
+
+    private Logger log = LoggerFactory.getLogger(QaSchemaTest.class);
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    @Rollback(value = false)
+    public void test() {
+    }
+
+}
