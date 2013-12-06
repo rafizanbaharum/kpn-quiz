29e5ba97174f4688e67a3cb379747b21a5f13d55
Rafizan Uda Baharum <rafizan.baharum@gmail.com>
2013-12-06 03:54:05 +0000

    Fixed

--- kpn-quiz-lib/src/test/resources/data/principal.sql
+++ kpn-quiz-lib/src/test/resources/data/principal.sql
@@ -14,9 +14,9 @@ insert into QA_PCPL (ID, NAME, PRINCIPAL_TYPE, ENABLED, LOCKED, M_ST, C_ID, C_TS
 insert into QA_USER (ID, REALNAME, PASSWORD, EMAIL, ACTOR_ID)
 values (0, 'System Root', 'abc123', 'rafizan.baharum@gmail.com', null);
 insert into QA_ACTR (ID, ACTOR_TYPE, NAME, NRIC_NO, ADDRESS1, ADDRESS2, ADDRESS3, AGE, EMAIL, FAX, M_ST, C_ID, C_TS)
-values (0, 2, 'System Root', '000000000000', 'Lorong 1 ', 'Taman Midah', 'Cheras', 22, 'rafizan.baharum@gmail.com', 'fax', 1, 1, CURRENT_TIMESTAMP );
+values (0, 2, 'System Root', '000000000000', 'Kementerian Penerangan', 'Putrajaya', 'WPKL', 22, 'rafizan.baharum@gmail.com', 'fax', 1, 1, CURRENT_TIMESTAMP );
 insert into QA_SPPT(ID) values(0);
-update QA_USER set ACTOR_ID = 0 where id = 1;
+update QA_USER set ACTOR_ID = 0 where id = 0;
 insert into QA_PCPL_ROLE (ID, ROLE_TYPE, PRINCIPAL_ID, M_ST, C_ID, C_TS) values(1, 0, 1, 1, 1, CURRENT_TIMESTAMP );
 insert into QA_PCPL_ROLE (ID, ROLE_TYPE, PRINCIPAL_ID, M_ST, C_ID, C_TS) values(2, 1, 1, 1, 1, CURRENT_TIMESTAMP );
 
@@ -25,8 +25,26 @@ insert into QA_PCPL (ID, NAME, PRINCIPAL_TYPE, ENABLED, LOCKED, M_ST, C_ID, C_TS
 insert into QA_USER (ID, REALNAME, PASSWORD, EMAIL, ACTOR_ID)
 values (1, 'Support', 'abc123', 'rafizan.baharum@gmail.com', null);
 insert into QA_ACTR (ID, ACTOR_TYPE, NAME, NRIC_NO, ADDRESS1, ADDRESS2, ADDRESS3, AGE, EMAIL, FAX, M_ST, C_ID, C_TS)
-values (1, 2, 'System Support I', '012345678912',  'Lorong 1 ', 'Taman Midah', 'Cheras', 22, 'rafizan.baharum@gmail.com', 'fax', 1, 1, CURRENT_TIMESTAMP );
+values (1, 2, 'System Support I', '1111111111',  'Kementerian Penerangan', 'Putrajaya', 'WPKL', 22, 'rafizan.baharum@gmail.com', 'fax', 1, 1, CURRENT_TIMESTAMP );
 insert into QA_SPPT(ID) values(1);
 update QA_USER set ACTOR_ID = 1 where id = 1;
 insert into QA_PCPL_ROLE (ID, ROLE_TYPE, PRINCIPAL_ID, M_ST, C_ID, C_TS) values(3, 1, 1, 1, 1, CURRENT_TIMESTAMP );
-insert into QA_PCPL_ROLE (ID, ROLE_TYPE, PRINCIPAL_ID, M_ST, C_ID, C_TS) values(3, 1, 1, 1, 1, CURRENT_TIMESTAMP );
+insert into QA_PCPL_ROLE (ID, ROLE_TYPE, PRINCIPAL_ID, M_ST, C_ID, C_TS) values(4, 1, 1, 1, 1, CURRENT_TIMESTAMP );
+
+
+insert into QA_PCPL (ID, NAME, PRINCIPAL_TYPE, ENABLED, LOCKED, M_ST, C_ID, C_TS) values (2, '222222222222', 0, true, true, 1, 0, CURRENT_TIMESTAMP );
+insert into QA_USER (ID, REALNAME, PASSWORD, EMAIL, ACTOR_ID)
+values (2, 'Support', 'abc123', 'rafizan.baharum@gmail.com', null);
+insert into QA_ACTR (ID, ACTOR_TYPE, NAME, NRIC_NO, ADDRESS1, ADDRESS2, ADDRESS3, AGE, EMAIL, FAX, M_ST, C_ID, C_TS)
+values (2, 2, 'System Support II', '222222222222',  'Kementerian Penerangan', 'Putrajaya', 'WPKL', 22, 'rafizan.baharum@gmail.com', 'fax', 1, 1, CURRENT_TIMESTAMP );
+insert into QA_SPPT(ID) values(2);
+update QA_USER set ACTOR_ID = 2 where id = 2;
+insert into QA_PCPL_ROLE (ID, ROLE_TYPE, PRINCIPAL_ID, M_ST, C_ID, C_TS) values(5, 1, 1, 1, 1, CURRENT_TIMESTAMP );
+insert into QA_PCPL_ROLE (ID, ROLE_TYPE, PRINCIPAL_ID, M_ST, C_ID, C_TS) values(6, 1, 1, 1, 1, CURRENT_TIMESTAMP );
+
+setval('SEQ_QA_PCPL', 3, true);
+setval('SEQ_QA_ACTR', 3, true);
+setval('SEQ_QA_PCPL_ROLE', 7, true);
+
+
+
