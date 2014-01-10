5081dafc432782a5f6ed60644ed8b5d2a890d33b
Faizal Abdul Manan <faizal.manan@gmail.com>
2014-01-10 11:15:11 +0800

    disable concurrent session management

--- kpn-quiz-admin/src/main/java/my/gov/kpn/quiz/web/config/QaWebSecurityConfig.java
+++ kpn-quiz-admin/src/main/java/my/gov/kpn/quiz/web/config/QaWebSecurityConfig.java
@@ -57,12 +57,12 @@ public class QaWebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout()
                 .logoutUrl("/logout")
                 .logoutSuccessUrl("/index")
-                .invalidateHttpSession(true)
-                .and()
-                .sessionManagement()
-                .maximumSessions(1)
-                .maxSessionsPreventsLogin(true)
-                .expiredUrl("/gate/in");
+                .invalidateHttpSession(true);
+//                .and()
+//                .sessionManagement()
+//                .maximumSessions(1)
+//                .maxSessionsPreventsLogin(true)
+//                .expiredUrl("/gate/in");
     }
 
 
