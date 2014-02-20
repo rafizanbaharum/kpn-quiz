12f593382ac77ce942d1e8fd87ac6ca9d19e909b
Rafizan Baharum <rafizan.baharum@gmail.com>
2014-02-21 00:59:42 +0800

    Fixed deploy script

--- kpn-quiz-hub/src/main/assembly/cluster/cluster-deploy.sh
+++ kpn-quiz-hub/src/main/assembly/cluster/cluster-deploy.sh
@@ -3,5 +3,5 @@ export CLUSTER_HOME=/app/cluster
 for i in {0..9}
 do
 rm -rf $CLUSTER_HOME/t$i/webapps/ROOT
-cp -v /app/GitHub/kpn-quiz/kpn-quiz-hub/target/ROOT.war $CLUSTER_HOME/t$i/webapps/.
+cp -v /app/GitHub/kpn-quiz/kpn-quiz-hub/target/kpn-quiz-hub.war $CLUSTER_HOME/t$i/webapps/ROOT.war
 done
