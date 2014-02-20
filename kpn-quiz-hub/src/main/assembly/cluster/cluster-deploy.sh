1912369228536da2b68e52a24bce511a0868f953
Rafizan Baharum <rafizan.baharum@gmail.com>
2014-02-21 00:52:00 +0800

    Fixed for ROOT deploy

--- kpn-quiz-hub/src/main/assembly/cluster/cluster-deploy.sh
+++ kpn-quiz-hub/src/main/assembly/cluster/cluster-deploy.sh
@@ -2,6 +2,6 @@ export CLUSTER_HOME=/app/cluster
 
 for i in {0..9}
 do
-rm -rf $CLUSTER_HOME/t$i/webapps/kpn-quiz-hub
-cp -v /app/GitHub/kpn-quiz/kpn-quiz-hub/target/kpn-quiz-hub.war $CLUSTER_HOME/t$i/webapps/.
+rm -rf $CLUSTER_HOME/t$i/webapps/ROOT
+cp -v /app/GitHub/kpn-quiz/kpn-quiz-hub/target/ROOT.war $CLUSTER_HOME/t$i/webapps/.
 done
