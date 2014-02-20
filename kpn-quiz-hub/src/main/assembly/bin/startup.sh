94ba95df213f8606336cddfc6114792556de6b91
Rafizan Baharum <rafizan.baharum@gmail.com>
2014-02-20 13:17:25 +0800

    Added script

---
+++ kpn-quiz-hub/src/main/assembly/bin/startup.sh
@@ -0,0 +1,5 @@
+export CATALINA_BASE=/cluster/t0
+export CATALINA_HOME=/usr/local/apache-tomcat/apache-tomcat-7.0.52
+export CATALINA_OPTS=-server -Xms1024m -Xmx3072m
+$CATALINA_HOME\bin\startup.sh
+
