f76fb7c86e6c11a20049690d6d21539b1ba45fce
Rafizan Baharum <rafizan.baharum@gmail.com>
2014-02-20 18:45:36 +0800

    /app prefix

--- kpn-quiz-hub/src/main/assembly/bin/startup.sh
+++ kpn-quiz-hub/src/main/assembly/bin/startup.sh
@@ -1,4 +1,4 @@
-export CATALINA_BASE=/cluster/t0
+export CATALINA_BASE=/app/cluster/t0
 export CATALINA_HOME=/usr/local/apache-tomcat/apache-tomcat-7.0.52
 export CATALINA_OPTS="-server -Xms1024m -Xmx3072m"
 $CATALINA_HOME/bin/startup.sh
