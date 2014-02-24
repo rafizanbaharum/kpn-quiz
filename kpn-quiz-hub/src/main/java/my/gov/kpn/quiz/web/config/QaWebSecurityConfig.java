3813f9e99cb37dfd8265c5ff3c4fa7d080b30a3d
Rafizan Baharum <rafizan.baharum@gmail.com>
2014-02-24 12:38:08 +0800

    Fixed session registry

--- kpn-quiz-hub/src/main/java/my/gov/kpn/quiz/web/config/QaWebSecurityConfig.java
+++ kpn-quiz-hub/src/main/java/my/gov/kpn/quiz/web/config/QaWebSecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -62,9 +61,9 @@ public class QaWebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutUrl("/logout")
                 .logoutSuccessUrl("/index.jsp")
                 .invalidateHttpSession(true)
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//                .and()
+//                .sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 //                .maximumSessions(1)
 //                .sessionRegistry(sessionRegistry())
 //                .expiredUrl("/gate/in")
