83bcd4b6094be23f51a14814f54cda171e436bd9
Rafizan Baharum <rafizan.baharum@gmail.com>
2014-02-24 16:31:28 +0800

    Added catalina opts activiti check

--- kpn-quiz-hub/src/main/assembly/cluster/t9/bin/startup.sh
+++ kpn-quiz-hub/src/main/assembly/cluster/t9/bin/startup.sh
@@ -1,5 +1,5 @@
 export CATALINA_BASE=/app/cluster/t9
 export CATALINA_HOME=/usr/local/apache-tomcat/apache-tomcat-7.0.52
-export CATALINA_OPTS="-server -Xms1024m -Xmx3072m"
+export CATALINA_OPTS="-server -Xms1024m -Xmx3072m -Dorg.apache.catalina.session.StandardSession.ACTIVITY_CHECK=true"
 $CATALINA_HOME/bin/startup.sh
 
