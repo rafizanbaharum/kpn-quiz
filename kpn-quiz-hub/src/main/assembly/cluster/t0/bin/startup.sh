592cfc08f0837b45b49db9f33480a17d51f3b2db
Faizal Abdul Manan <faizal.manan@gmail.com>
2014-02-24 16:48:28 +0800

    update jvm config

--- kpn-quiz-hub/src/main/assembly/cluster/t0/bin/startup.sh
+++ kpn-quiz-hub/src/main/assembly/cluster/t0/bin/startup.sh
@@ -1,5 +1,6 @@
 export CATALINA_BASE=/app/cluster/t0
 export CATALINA_HOME=/usr/local/apache-tomcat/apache-tomcat-7.0.52
-export CATALINA_OPTS="-server -Xms1024m -Xmx3072m -Dorg.apache.catalina.session.StandardSession.ACTIVITY_CHECK=true"
+#export CATALINA_OPTS="-server -Xms3072m -Xmx3072m -Dorg.apache.catalina.session.StandardSession.ACTIVITY_CHECK=true"
+export CATALINA_OPTS="-server -Xmx2g –Xms2g -XX:MaxNewSize=512m -XX:MaxPermSize=512m  -XX:ParallelGCThreads=4 -Dorg.apache.catalina.session.StandardSession.ACTIVITY_CHECK=true"
 $CATALINA_HOME/bin/startup.sh
 
