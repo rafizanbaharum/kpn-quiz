77bbac4201d5a4d95bdbb1d87154a2363c466639
Faizal Abdul Manan <faizal.manan@gmail.com>
2014-03-11 21:21:27 +0800

    cleanup test file

--- kpn-quiz-admin/src/test/java/my/gov/kpn/quiz/TestAppConfig.java
+++ kpn-quiz-admin/src/test/java/my/gov/kpn/quiz/TestAppConfig.java
@@ -1,9 +1,17 @@
 package my.gov.kpn.quiz;
 
+import org.apache.commons.dbcp.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
 import org.springframework.transaction.PlatformTransactionManager;
